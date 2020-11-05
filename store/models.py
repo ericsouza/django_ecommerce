72bffd7338024402c9e7a9be749299b6af47c92c
Eric Souza <ericsouza0801@gmail.com>
2020-11-05 19:58:45 -0300

    rendering data to cart and checkout pages

--- store/models.py
+++ store/models.py
@@ -41,6 +41,16 @@ class Order(models.Model):
     def __str__(self):
         return str(self.id)
 
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        return sum([item.get_total for item in orderitems])
+
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        return sum([item.quantity for item in orderitems])
+
 
 class OrderItem(models.Model):
     product = models.ForeignKey(
@@ -49,6 +59,10 @@ class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True, blank=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def get_total(self):
+        return self.product.price * self.quantity
 
 
 class ShippingAddress(models.Model):
